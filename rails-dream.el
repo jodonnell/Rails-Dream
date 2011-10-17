805e0e9e5fc336fc7c0d7079f95bb55c33346d8b
Jacob O'Donnell <jacobodonnell@gmail.com>
2011-10-17 01:07:06 -0400

    create rails buffer and some further shrinking

--- rails-dream.el
+++ rails-dream.el
@@ -1,12 +1,18 @@
-(defun open-rails-console-buffer ()
+(defun create-rails-console-buffer-if-does-not-exist ()
+  (if (not (get-buffer "rails-console-buffer"))
+      (open-rails-console-buffer)))
+
+(defun create-rails-console-buffer ()
   (let (old-buffer)
     (setq old-buffer (current-buffer))
     (shell "rails-console-buffer")
-    (sit-for 1)
-    (insert "rails c")
-    (comint-send-input)
-    (sit-for 12)
     (switch-to-buffer old-buffer)))
+
+(defun open-rails-console-buffer ()
+  (create-rails-console-buffer)
+  (sit-for 1)
+  (process-send-string "rails-console-buffer" "rails c\n")
+  (sit-for 8))
 
 (defun get-class ()
   (save-excursion
@@ -23,24 +29,20 @@
 (defun token-regexp ()
   "[_A-Za-z0-9]+")
 
-(defun get-instance-methods (class-name)
-  (get-methods class-name "instance_methods"))
-
-(defun get-class-methods (class-name)
-  (get-methods class-name "methods"))
-
 (defun get-rails-console-clean-method-output ()
-  (beginning-of-buffer)
-  (next-line)
-  (next-line)
-  (beginning-of-line)
+  (save-excursion
+    (set-buffer "rails-console-buffer")
+    (beginning-of-buffer)
+    (next-line)
+    (next-line)
+    (beginning-of-line)
 
-  (let (begin-of-methods-pos)
-    (setq begin-of-methods-pos (point))
-    (search-forward ">")
-    (previous-line)
-    (end-of-line)
-    (buffer-substring begin-of-methods-pos (point))))
+    (let (begin-of-methods-pos)
+      (setq begin-of-methods-pos (point))
+      (search-forward ">")
+      (previous-line)
+      (end-of-line)
+      (buffer-substring begin-of-methods-pos (point)))))
 
 (defun get-methods (class-name method-type)
   (save-excursion
@@ -51,17 +53,16 @@
     (process-send-string "rails-console-buffer" (concat "(" class-name "." method-type " - Object.methods).sort.collect {|method| puts method.to_s}\n"))
     (sit-for 2)
 
-    (setq list-of-methods (get-rails-console-clean-method-output))
     (switch-to-buffer (get-buffer-create (concat class-name " " method-type)))
     (erase-buffer)
-    (insert list-of-methods)
+    (insert (get-rails-console-clean-method-output))
     (beginning-of-buffer)))
 
+(defun get-instance-methods (class-name)
+  (get-methods class-name "instance_methods"))
 
-(defun create-rails-console-buffer-if-does-not-exist ()
-  (if (not (get-buffer "rails-console-buffer"))
-      (open-rails-console-buffer)))
-
+(defun get-class-methods (class-name)
+  (get-methods class-name "methods"))
 
 (defun get-class-methods-current ()
   (interactive)
