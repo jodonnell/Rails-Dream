bd6a2a1c8db14cd59dfa54f29a59dad94fd3e544
Jacob O'Donnell <jacobodonnell@gmail.com>
2011-10-20 00:23:32 -0400

    pulled out call-interactive-shell-command

--- rails-dream.el
+++ rails-dream.el
@@ -1,18 +1,18 @@
 (defun create-console-buffer-if-does-not-exist (buffer-name command)
+  (defun open-console-buffer ()
+    (create-console-buffer)
+    (sit-for 1)
+    (process-send-string buffer-name command)
+    (sit-for 8))
+
+  (defun create-console-buffer ()
+    (let (old-buffer)
+      (setq old-buffer (current-buffer))
+      (shell buffer-name)
+      (switch-to-buffer old-buffer)))
+
   (if (not (get-buffer buffer-name))
-      (open-console-buffer buffer-name command)))
-
-(defun open-console-buffer (buffer-name command)
-  (create-console-buffer buffer-name)
-  (sit-for 1)
-  (process-send-string buffer-name command)
-  (sit-for 8))
-
-(defun create-console-buffer (buffer-name)
-  (let (old-buffer)
-    (setq old-buffer (current-buffer))
-    (shell buffer-name)
-    (switch-to-buffer old-buffer)))
+      (open-console-buffer)))
 
 (defun get-class ()
   (save-excursion
@@ -44,14 +44,19 @@
       (end-of-line)
       (buffer-substring begin-of-methods-pos (point)))))
 
+
+(defun call-interactive-shell-command (buffer start-interactive-shell-command interactive-shell-command)
+  "Use to call an interactive shell command, first call will create the buffer and open the command, all calls will run the command you pass in interactive-shell-command"
+  (create-console-buffer-if-does-not-exist buffer start-interactive-shell-command)
+  (set-buffer buffer)
+  (erase-buffer)
+
+  (process-send-string buffer interactive-shell-command)
+  (sit-for 2))
+
 (defun get-methods (class-name method-type)
   (save-excursion
-    (create-console-buffer-if-does-not-exist "rails-console-buffer" "rails c\n")
-    (set-buffer "rails-console-buffer")
-    (erase-buffer)
-
-    (process-send-string "rails-console-buffer" (concat "(" class-name "." method-type " - Object.methods).sort.collect {|method| puts method.to_s}\n"))
-    (sit-for 2)
+    (call-interactive-shell-command "rails-console-buffer" "rails c\n" (concat "(" class-name "." method-type " - Object.methods).sort.collect {|method| puts method.to_s}\n"))
 
     (switch-to-buffer (get-buffer-create (concat class-name " " method-type)))
     (erase-buffer)
@@ -72,8 +77,9 @@
   (interactive)
   (get-instance-methods (get-class)))
 
-;(open-rails-console-buffer)
+; (create-console-buffer-if-does-not-exist "rails-console-buffer" "rails c\n")
 ;(get-instance-methods "ActionController::Base")
 ;(get-class-methods "ActionController::Base")
 ;(get-instance-methods "HoneyPieController")
+
 
