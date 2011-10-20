ce65175c505306848d2961b68b2eda579c9b3bff
Jacob O'Donnell <jacobodonnell@gmail.com>
2011-10-20 01:20:13 -0400

    playing around with testing

--- rails-dream.el
+++ rails-dream.el
@@ -1,3 +1,5 @@
+;  "class User"
+
 (defun create-console-buffer-if-does-not-exist (buffer-name command)
   (defun open-console-buffer ()
     (create-console-buffer)
@@ -75,4 +77,18 @@
 ;(get-class-methods "ActionController::Base")
 ;(get-instance-methods "HoneyPieController")
 
+; (call-interactive-shell-command "ri-console-buffer" "ri -i -T\n" "form_tag\n")
+
+(deftest "testing methods"
+
+  (cd "/Users/jacobodonnell/programming/the_honey_pie_star_chart")
+  (get-instance-methods-current)
+
+  (let (old-buffer save-found)
+    (setq old-buffer (current-buffer))
+    (set-buffer "User instance_methods")
+    (setq save-found (search-forward "save!" nil t)
+    (switch-to-buffer old-buffer))
+    
+    (assert-nonnil save-found)))
 
